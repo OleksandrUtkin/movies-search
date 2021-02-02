1352b09fabb34a95d27484ae6cf388e53b23aeed
Oleksandr Utkin <neoleksandrutkin@gmail.com>
2021-02-03 00:37:44 +0200

    add sort container

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sort from "./components/Sort";
+
+function App() {
+    return (
+        <div className="sort-wrap">
+            <Sort/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Sort.jsx
@@ -0,0 +1,70 @@
+import React, {useState, useEffect, useRef} from 'react';
+
+const Sort = (props) => {
+    const [sortByValue, setSortByValue] = useState(null);
+    const [showSortDropDown, setShowSortDropDown] = useState(false);
+    const sortByList = ['Thriller', 'Action', 'Comedy', 'Detective'];
+    const sortValueRef = useRef(null);
+    const clearSortRef = useRef(null);
+
+    const onSortByItemClick = (genre) => {
+        setSortByValue(genre);
+        setShowSortDropDown(false);
+    };
+
+    // open sort dropdown && clear sort
+    useEffect(() => {
+        const showSortDropDownFunc = (event) => {
+            setShowSortDropDown(!showSortDropDown);
+        };
+
+        const clearSort = () => {
+            setSortByValue(null);
+        };
+
+        const clickOnSortValue = (event) => {
+            clearSortRef.current && clearSortRef.current.contains(event.target) ? clearSort() : showSortDropDownFunc();
+        };
+
+        const sortValueContainer = document.querySelector('.sort__value');
+        sortValueContainer && sortValueContainer.addEventListener('click', clickOnSortValue);
+
+        return () => sortValueContainer && sortValueContainer.removeEventListener("click", clickOnSortValue);
+    }, [sortByValue, showSortDropDown]);
+
+
+    // hide dropdown on outside click
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            !sortValueRef.current.contains(event.target) && showSortDropDown && setShowSortDropDown(false);
+        };
+
+        document.addEventListener("click", handleClickOutside);
+        return () => document.removeEventListener("click", handleClickOutside);
+    }, [showSortDropDown]);
+
+
+    return (
+        <div className='sort'>
+            <div className='sort__value' ref={sortValueRef}>
+                <span>{sortByValue ? sortByValue : 'Sort by'}</span>
+                {sortByValue &&
+                <div className="sort__clear" ref={clearSortRef}>
+                    <div></div>
+                    <div></div>
+                </div>}
+            </div>
+            {showSortDropDown && <ul className='sort__dropdown'>
+                {sortByList.map((genre, index) =>
+                    <li
+                        key={genre + index}
+                        onClick={() => onSortByItemClick(genre)}>
+                        {genre}
+                    </li>
+                )}
+            </ul>}
+        </div>
+    );
+};
+
+export default Sort;
